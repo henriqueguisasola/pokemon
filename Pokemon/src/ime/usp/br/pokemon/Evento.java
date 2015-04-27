62b7d99d0582d026f1168671bb6f23637db96d92
mac0321 08 <mac0321.08@hulk.cec.ime.usp.br>
2015-04-27 08:53:00 -0300

    Código base

---
+++ Pokemon/src/ime/usp/br/pokemon/Evento.java
@@ -0,0 +1,13 @@
+package ime.usp.br.pokemon;
+
+public class Evento {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Pokemon/src/ime/usp/br/pokemon/Treinador.java
@@ -0,0 +1,8 @@
+package ime.usp.br.pokemon;
+
+public class Treinador {
+
+	private int hp = 0;
+	private String nome;
+	
+}
