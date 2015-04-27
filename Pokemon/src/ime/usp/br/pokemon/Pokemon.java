0fcaa1da8e43a4309c419855b459518e1c76ec43
mac0321 14 <mac0321.14@hulk.cec.ime.usp.br>
2015-04-27 09:42:09 -0300

    Adicionei Habilidade e modifiquei Pokemon

--- Pokemon/src/ime/usp/br/pokemon/Pokemon.java
+++ Pokemon/src/ime/usp/br/pokemon/Pokemon.java
@@ -1,13 +1,19 @@
 package ime.usp.br.pokemon;
 
 public class Pokemon {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int hp;
+	private int i = 0;
+	private String nome;
+	private Habilidade[] habilidades = new Habilidade[4];
+	public Pokemon(String n, int vida){
+		nome = n;
+		hp = vida;
 	}
-
+	public void setHabil(Habilidade a){
+		habilidades[i] = a;
+		i++;
+	}
+	public String getNome(){
+		return nome;
+	}
 }
