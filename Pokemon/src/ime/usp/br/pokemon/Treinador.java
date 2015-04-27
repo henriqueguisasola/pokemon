e52193bc29bce765bc2cb7650c03519b918c6299
mac0321 14 <mac0321.14@hulk.cec.ime.usp.br>
2015-04-27 09:49:32 -0300

    Modifiquei Treinador

--- Pokemon/src/ime/usp/br/pokemon/Treinador.java
+++ Pokemon/src/ime/usp/br/pokemon/Treinador.java
@@ -1,8 +1,15 @@
 package ime.usp.br.pokemon;
 
 public class Treinador {
-
+	private Pokemon[] pokemons = new Pokemon[6];
 	private int hp = 0;
+	private int i = 0;
 	private String nome;
-	
+	public Treinador(String n){
+		nome = n;
+	}
+	public void setPokemons(Pokemon a){
+		pokemons[i] = a;
+		i++;
+	}
 }
