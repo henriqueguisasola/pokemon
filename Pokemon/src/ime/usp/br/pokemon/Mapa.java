6382e2ea5241bc255b79a7b648b1b7f6741dbf71
MauImaeda <Mauricio@Mauricios-MacBook-Air.local>
2015-05-05 20:20:43 -0300

    modifiquei mapa criei novas funções

--- Pokemon/src/ime/usp/br/pokemon/Mapa.java
+++ Pokemon/src/ime/usp/br/pokemon/Mapa.java
@@ -15,15 +15,34 @@ public class Mapa {
 	public void imprimeMapa(){
 		for (int i =0 ; i < 10; i++){
 			for(int j = 0; j < 10; j++){
-				if (m[i][j] != 2)
-					System.out.print(m[i][j] +" ");
-				else
+				if (m[i][j] == 1){
+					System.out.print("# ");
+				}
+				if(m[i][j] == 0){
+					System.out.print("0 ");
+				}
+				if(m[i][j] == 2){
 					System.out.print("T ");
+				}	
 			}
 			System.out.println("");
 		}
 	}
 	public void AtualizarTreinadorMapa(Treinador t){
 		m[t.getAltura()][t.getLargura()] = 2;
+	}
+	public boolean temPokemon(Treinador t){
+		if (m[t.getAltura()][t.getLargura()] == 1){
+			if(Math.random() >= 0.4){
+				//Chance de ter um pokemon
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
 	}
 }
